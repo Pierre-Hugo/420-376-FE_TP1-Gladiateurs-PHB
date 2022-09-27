babac99cf8cb0c3b50c6a0cc9d998f8d4b0194c6
ETI <ETI@PO-21499.cogeco.local>
2022-09-27 13:57:56 -0400

    Étape 9 finalisée

--- JeuGladiateurs/src/combat/CompteurDeTour.java
+++ JeuGladiateurs/src/combat/CompteurDeTour.java
@@ -41,6 +41,7 @@ public class CompteurDeTour {
     }
 
     public void afficheTour() {
+        System.out.println("");
         System.out.println("---------------");
         System.out.println("TOUR ACTUEL : " + cptrTour);
         System.out.println("---------------");
--- JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
+++ JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
@@ -22,8 +22,6 @@ public class JeuGladiateurs {
     // **************************************************************************
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Affichage pré-combat">
-    personnage1.afficherInfosPersonnage();
-    personnage2.afficherInfosPersonnage();
     affichage.afficherDebutCombat();
     
     // </editor-fold>
@@ -32,8 +30,24 @@ public class JeuGladiateurs {
     // **************************************************************************
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Mécanique de combat">
-    // TODO : La boucle contenant les étapes du combat
-    // TODO : Après la boucle, afficher le résultat du combat
+        do {
+            tour.afficheTour();
+            for (int i = 0; i < 100; i++) {
+                if (personnage1.getInitiative() == i) {
+                    personnage1.frapperPersonnage(personnage2);
+                }
+                if (personnage2.getInitiative() == i) {
+                    personnage2.frapperPersonnage(personnage1);
+                }
+            }
+            affichage.afficherSeparateurInfosPerso();
+            personnage1.afficherInfosPersonnage();
+            personnage2.afficherInfosPersonnage();
+            personnage1.setNewInitiativeRandom();
+            personnage2.setNewInitiativeRandom();
+            tour.augmenteTour();
+            affichage.afficherSeparateurDeTour();
+        } while (personnage1.getPointsDeVie() > 0 && personnage2.getPointsDeVie() > 0);
     // </editor-fold>
     }
 
